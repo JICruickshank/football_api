f9258c4ac3e0104ab3576998868078bc6818d0ea
Jonathan Cruickshank <ikenna1982@hotmail.com>
2018-04-13 15:05:28 +0100

    adds attempt at pop up image

--- public/index.js
+++ public/index.js
@@ -5,9 +5,8 @@ const app = function(){
   request.setRequestHeader("X-Auth-Token", "ce59c6fd7c2d47c29fb4c133e01112d8");
   request.addEventListener("load", function() {
     const parsedTable = JSON.parse(request.responseText).standing;
-    debugger;
     displayTable(parsedTable);
-
+    displayTeamBadge();
   });
   request.send();
 }
@@ -17,7 +16,6 @@ const createTable = function(id, headers) {
   table.id = id;
   const headerRow = document.createElement("tr");
   table.appendChild(headerRow);
-  debugger;
   for(let header of headers) {
     const tableHeader = document.createElement("th");
     tableHeader.innerText = header;
@@ -31,13 +29,13 @@ const displayTable = function(table) {
   const container = document.querySelector("#leagueTableDiv");
   const headers = ["", "Team", "P", "W", "D", "L", "F", "A", "Pts", "GD"];
   const leagueTable = createTable("leagueTable", headers);
-  debugger;
   container.appendChild(leagueTable);
   // loop through each team, create a table row
   // loop through team stats, creating a cell and appending to table row
   // append row to table
   for(let team of table) {
     const tableRow = document.createElement("tr");
+    tableRow.id = team.teamName;
     const teamStats = [team.position, team.teamName, team.playedGames, team.wins, team.draws, team.losses, team.goals, team.goalsAgainst, team.points, team.goalDifference];
     for(let stat of teamStats) {
       const cellData = document.createElement("td");
@@ -48,6 +46,32 @@ const displayTable = function(table) {
   }
   return leagueTable;
 };
+
+const displayTeamBadge = function() {
+  const table = document.getElementById("leagueTable");
+  debugger;
+  table.addEventListener("onmouseover", function(event) {
+    console.log(event);
+
+  })
+}
+
+const createImage = function(url) {
+  const img = document.createElement("img");
+  img.src = url;
+}
+
+// const createPopUpBox = function(id, img) {
+//   const container = document.getElementById("popUp");
+//   const url =
+//   container.appendChild(img);
+
+
+
+
+  // }
+
+
 
 
 
